a7731bddd9dbdfa65b706e7f74da9b9166680e7a
Vincent zhou <vincentzhou@Vincentdebijibendiannao.local>
2024-10-15 14:50:43 +0800

    draw first triangle

---
+++ Metal_graphics/definitions.h
@@ -0,0 +1,20 @@
+//
+//  definitions.h
+//  Metal_graphics
+//
+//  Created by Vincent zhou on 2024/10/15.
+//
+
+#ifndef definitions_h
+#define definitions_h
+
+#include <simd/simd.h>
+
+struct Vertex{
+    vector_float2 position;
+    vector_float4 color;
+};
+
+#endif /* definitions_h */
+
+
